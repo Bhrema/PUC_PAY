785ea69157bb01e8b38134afe7d7a22915325145
Otávio Costa Dallo <106034358+otaviodallo@users.noreply.github.com>
2023-05-11 20:54:55 -0300

    restaurant config added

---
+++ user.entity.ts
@@ -0,0 +1,50 @@
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  name: string;
+
+  @Column( { nullable: true } )
+  cpf: string;
+
+  @Column( { nullable: true } )
+  cnpj: string;
+
+  @Column( { nullable: true } )
+  block: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: false })
+  isAdmin: boolean;
+  
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id);
+  }
+}
