9ca5cc60e601382a3a2354179cffcc5e99b42bd5
Luiz Gustavo Chimenes <gustavochimenes@hotmail.com>
2023-05-22 20:01:28 -0300

    dtos

---
+++ src/users/decorators/dtos/update-user.dto.ts
@@ -0,0 +1,31 @@
+import { IsEmail, IsString, IsOptional, IsBoolean } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsEmail()
+  @IsOptional()
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  password: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isAdmin: boolean;
+
+  @IsString()
+  @IsOptional()
+  block: string;
+
+  @IsString()
+  @IsOptional()
+  cpf: string
+
+  @IsString()
+  @IsOptional()
+  cnpj: string
+}
---
+++ src/users/decorators/dtos/user.dto.ts
@@ -0,0 +1,27 @@
+import { Expose } from 'class-transformer';
+
+export class UserDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  name: string;
+
+  @Expose()
+  email: string;
+
+  @Expose()
+  cpf: string;
+
+  @Expose()
+  cnpj: string;
+
+  @Expose()
+  block: string;
+
+  @Expose()
+  image: string;
+
+  @Expose()
+  isAdmin: boolean;
+}
